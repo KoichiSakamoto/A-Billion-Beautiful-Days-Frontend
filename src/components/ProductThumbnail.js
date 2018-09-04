0e432c50d17d82426b1a2bc59631e57fb819e024
KoichiSakamoto <isaac.fiore@mail.mcgill.ca>
2018-09-04 18:03:29 -0400

    updated productthumbnail component

--- src/components/ProductThumbnail.js
+++ src/components/ProductThumbnail.js
@@ -5,6 +5,8 @@ const ProductThumbnail = (props) =>
   <div className="prod-thumbnail">
     <p> {props.product.name} </p>
     <p> Price: {props.product.price} $ </p>
+    <img src={props.product.thumbnail_url} alt="thumbnail" />
+    <hr />
   </div>;
 
 
