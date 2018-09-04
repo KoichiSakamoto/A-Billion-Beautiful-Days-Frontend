a7d406c5545f54f3a8bd84d2156d10b95a3c34e2
KoichiSakamoto <isaac.fiore@mail.mcgill.ca>
2018-09-04 15:16:29 -0400

    created website-about-blurb component

---
+++ src/components/WebsiteAboutBlurb.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+
+
+const WebsiteAboutBlurb = (props) =>
+  <div className="about-website">
+    <h3> Welcome To A Billion Beautiful Days! </h3>
+    <Link to="/all-products"> <button> Enter Site </button></Link> 
+  </div>;
+
+
+
+export default WebsiteAboutBlurb;
--- src/containers/WelcomePage.js
+++ src/containers/WelcomePage.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
-import Header from '../components/Header';
+import WebsiteAboutBlurb from '../components/WebsiteAboutBlurb';
 
 
 export default class WelcomePage extends Component {
 
   render() {
     return (<div>
-              <Header />
               <h1> Welcome Page </h1>
               <img src="placeholder_img.jpg" alt="placeholder" height="500"/>
-              <h3>Welcome to A Billion Beautiful Days! </h3>
+              <WebsiteAboutBlurb />
             </div>
           )}
 }
