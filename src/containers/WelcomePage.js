82f60097e22401b8c2bb2de530759170afdefce4
KoichiSakamoto <isaac.fiore@mail.mcgill.ca>
2018-09-04 15:10:47 -0400

    added image and about placeholders to welcome page

--- src/containers/WelcomePage.js
+++ src/containers/WelcomePage.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
 
+
 export default class WelcomePage extends Component {
 
   render() {
     return (<div>
               <Header />
               <h1> Welcome Page </h1>
+              <img src="placeholder_img.jpg" alt="placeholder" height="500"/>
+              <h3>Welcome to A Billion Beautiful Days! </h3>
             </div>
           )}
 }
