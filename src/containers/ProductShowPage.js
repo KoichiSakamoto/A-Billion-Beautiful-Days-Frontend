a414a5735d131b30758934f06e2ce2ff7fd453ee
KoichiSakamoto <isaac.fiore@mail.mcgill.ca>
2018-09-04 21:57:44 -0400

    add to cart feature done

--- src/containers/ProductShowPage.js
+++ src/containers/ProductShowPage.js
@@ -23,15 +23,28 @@ export default class ProductShowPage extends Component {
       <img src={img.storage_url} alt="blah" height="50" width="50" />)
   }
 
+  handleClick = () => {
+    fetch('http://localhost:4000/api/v1/cart_products', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({product_id: this.state.currentProduct.id, cart_id: 1})
+    })
+    .then(res => res.json())
+    .then(json => console.log(json))
+  }
+
   render() {
     return (
       <div>
         <Header />
         <h1> {this.state.currentProduct.name} </h1>
-        <h2> {this.state.currentProduct.price} </h2>
+        <h2> {this.state.currentProduct.price}.00 USD </h2>
         <h3> {this.state.currentProduct.about} </h3>
         <img src={this.state.currentProduct.thumbnail_url} alt="main img" />
         {this.renderProductImages()}
+        <button onClick={this.handleClick}> Add to Cart </button>
       </div>
     )
   }
