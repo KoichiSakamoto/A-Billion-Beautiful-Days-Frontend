b8d83a833239b6e00871b5b303095a3480c605e8
KoichiSakamoto <isaac.fiore@mail.mcgill.ca>
2018-09-10 10:52:21 -0400

    installed thunk, applied it to redux store

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers/index';
+import thunk from 'redux-thunk';
 
-let store = createStore(rootReducer)
+let store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
