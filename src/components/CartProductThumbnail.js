795d61a553a44f2a16d65e42587e481145d6ee8f
KoichiSakamoto <isaac.fiore@mail.mcgill.ca>
2018-09-24 18:57:09 -0400

    ability to go to product page from the cart thumbnail

--- src/components/CartProductThumbnail.js
+++ src/components/CartProductThumbnail.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const CartProductThumbnail = (props) =>
   <div className="cart-prod" data-price={props.product.price}>
-    <img src={props.product.thumbnail_url} alt="thumby" height="75" width="75" />
-    <span> {props.product.name}, </span>
+    <Link to={"/product/" + props.product.id}>
+      <img src={props.product.thumbnail_url} alt="thumby" height="75" width="75" />
+      <span> {props.product.name}, </span>
+    </Link>
     <span> {props.product.price} USD </span> <br />
     <button onClick={(e) => props.handleClick(e, props.cartProduct)}> Remove Item From Cart </button>
     <hr />
--- src/components/ProductThumbnail.js
+++ src/components/ProductThumbnail.js
@@ -6,7 +6,7 @@ const ProductThumbnail = (props) =>
   <div className="prod-thumbnail">
 
     <Link to={"/product/" + props.product.id} >
-      <img src={props.product.thumbnail_url} alt="thumbnail" height="300" width="100%"/>
+      <img src={props.product.thumbnail_url} alt="thumbnail" height="260" width="100%"/>
       <div className="center-text prod-name"> {props.product.name} </div>
       <div className="center-text"> {props.product.price} $ </div>
     </Link>
