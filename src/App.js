b1a8c74591f229be878c4385c841af966b8d50d1
KoichiSakamoto <isaac.fiore@mail.mcgill.ca>
2018-09-26 15:29:18 -0400

    created sizing information page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ProductIndexPage from './containers/ProductIndexPage.js'
 import ProductShowPage from './containers/ProductShowPage.js'
 import ViewCartPage from './containers/ViewCartPage.js'
 import WelcomePage from './containers/WelcomePage.js'
+import SizingInfo from './containers/SizingInfo.js'
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
           <Route path="" component={WelcomePage} exact/>
           <Route path="/all-products" component={ProductIndexPage} exact/>
           <Route path="/product" component={ProductShowPage} />
+          <Route path="/sizing" component={SizingInfo} />
           <Route path="/my-cart" component={ViewCartPage} exact/>
         </div>
       </BrowserRouter>
---
+++ src/containers/SizingInfo.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react';
+import Header from '../components/Header';
+
+
+
+export default class SizingInfo extends Component {
+
+  render() {
+    return ( <div>
+              <Header />
+              <main className="center-text">
+                <h2> TShirts </h2>
+                <table cellpadding="5" align="center">
+                  <tbody>
+                    <tr>
+                      <td>&nbsp;</td>
+                      <td><strong>XS</strong></td>
+                      <td><strong>S</strong></td>
+                      <td><strong>M</strong></td>
+                      <td><strong>L</strong></td>
+                      <td><strong>XL</strong></td>
+                      <td><strong>2XL</strong></td>
+                    </tr>
+                    <tr>
+                      <td><strong>Chest Width (inches) </strong></td>
+                      <td>16 3/8</td>
+                      <td>18 3/8</td>
+                      <td>20 3/8</td>
+                      <td>22 3/8</td>
+                      <td>24 3/8</td>
+                      <td>25</td>
+                    </tr>
+                    <tr>
+                      <td><strong>Body Length (inches) </strong></td>
+                      <td>28 1/2</td>
+                      <td>29 1/2</td>
+                      <td>30 3/8</td>
+                      <td>31 5/8</td>
+                      <td>32 5/8</td>
+                      <td>33</td>
+                    </tr>
+                    <tr>
+                      <td><strong>Sleeve Lenght (inches) </strong></td>
+                      <td>8</td>
+                      <td>8 3/8</td>
+                      <td>8 5/8</td>
+                      <td>9</td>
+                      <td>9 1/4</td>
+                      <td>9 1/4</td>
+                    </tr>
+                    </tbody>
+                  </table>
+
+                <h2> Tanktops </h2>
+                <table cellpadding="5" align="center">
+                  <tbody>
+                    <tr>
+                      <td>&nbsp;</td>
+                      <td><strong>XS</strong></td>
+                      <td><strong>L</strong></td>
+                      <td><strong>XL</strong></td>
+                      <td><strong>2XL</strong></td>
+                    </tr>
+                    <tr>
+                      <td><strong>Chest (inches) </strong></td>
+                      <td>30-32</td>
+                      <td>42-44</td>
+                      <td>46-48</td>
+                      <td>50-52</td>
+                    </tr>
+                    <tr>
+                      <td><strong>Waist (inches) </strong></td>
+                      <td>28-30</td>
+                      <td>33-34</td>
+                      <td>36-38</td>
+                      <td>40-42</td>
+                    </tr>
+                  </tbody>
+                </table>
+
+                <h2> Longsleeves </h2>
+                <table cellpadding="5" align="center">
+                  <tbody>
+                    <tr>
+                      <td>&nbsp;</td>
+                      <td><strong>S</strong></td>
+                      <td><strong>M</strong></td>
+                      <td><strong>L</strong></td>
+                      <td><strong>XL</strong></td>
+                      <td><strong>2XL</strong></td>
+                    </tr>
+                    <tr>
+                      <td><strong>Chest (inches) </strong></td>
+                      <td>34-36</td>
+                      <td>38-40</td>
+                      <td>42-44</td>
+                      <td>46-48</td>
+                      <td>48-50</td>
+                    </tr>
+                    <tr>
+                      <td><strong>Waist (inches) </strong></td>
+                      <td>30-32</td>
+                      <td>32-33</td>
+                      <td>33-34</td>
+                      <td>36-38</td>
+                      <td>40-42</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </main>
+             </div>
+          )}
+}
